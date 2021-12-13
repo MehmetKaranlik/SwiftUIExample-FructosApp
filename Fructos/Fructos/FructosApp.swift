a219f58bcc8482ff6cc35a0bc47ee3cc7e0ed48d
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-13 12:52:54 +0300

    First Commit

---
+++ Fructos/Fructos/FructosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructosApp.swift
+//  Fructos
+//
+//  Created by mehmet karanlık on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
